21e9b64bbe0f25834427f6f0083004d490a3720f
ChenAMing <cr32146342@live.com>
2022-09-29 21:29:12 +0800

    增加遮罩层组件

---
+++ src/lib/LibShade.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps<{ forReturn?: boolean }>()
+
+const emits = defineEmits<{ (event: 'click'): void }>()
+
+const onClick = () => emits('click')
+</script>
+
+<template>
+  <div
+    class="fixed inset-0"
+    :class="
+      props.forReturn
+        ? null
+        : 'bg-black bg-opacity-25  flex flex-col items-center justify-center backdrop-blur-sm'
+    "
+    @click="onClick"
+  >
+    <slot v-if="props.forReturn"></slot>
+  </div>
+</template>
